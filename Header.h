8da0ab685e92b5024c326a3c487d0e3a2f87b09e
ankit595 <56728798+ankit595@users.noreply.github.com>
2020-06-20 20:31:48 +0530

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,21 @@
+//Header for Project4
+int add(int a, int b) 
+{ 
+    printf("Added value=%d\n", a + b); 
+} 
+int sub(int a, int b) 
+{ 
+    printf("Substracted value=%d\n", a - b); 
+} 
+int multiply(int a, int b) 
+{ 
+    printf("Multiplied value=%d\n", a * b); 
+} 
+int divide(int a, int b) 
+{ 
+    printf("Divided value=%d\n", a / b); 
+} 
+int modulus(int a, int b) 
+{ 
+    printf("Remainder value=%d\n", a % b); 
+} 
